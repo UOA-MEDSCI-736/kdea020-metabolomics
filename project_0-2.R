6a66bf8867e2a65d20431a3bd4b60b052fe82619
kdea020 <kdea020@aucklanduni.ac.nz>
2016-08-18 12:30:37 +1200

    Adding a revised major iteration of project(0-2)

--- project_0-2.R
+++ project_0-2.R
@@ -1,8 +1,4 @@
 # - Basic instructions and data import
-#Eventually need to set working directory for portability
-
-# - If you just want to test the output, input ' mkSingleGraph(1) ' (where 1 may be replaced by the "number" of the compound you want to plot, 1-47)
-# - Or, you can now use ' mkGraphName("a string of text") ' and the script will plot what you entered, allowing multiple matches (and selection thereof). Not case sensitive.
 
 WD <- getwd()
 cat("Working directory is:", WD)
@@ -26,41 +22,21 @@ Type<-substr(rownames(Metab),1,1) #Grab first letter of rownames
 Type[Type=='X']<-'Sample' #Define as either C or S for each sample
 Type<-factor(Type) #levels?
 
-
-# - To get statistics for later use... etc.
-
-meanCompound <- t(apply(Metab, 2, mean))
-sdCompound <- t(apply(Metab, 2, sd))
-
 orderA <- InjOrder$injection.order
 maxnRows <- max(nRows)
 finalorderSamples <- c(orderA[1:maxnRows])
 
-
 # - Sort out batches and injection order stuff
-nBatch=4 #just for now, see next line
-#nBatch = SOMETHINGFROM InjOrder (i.e. gap in times >=2h?)
-#MetabInjOrder <- #need to get injection order and stuff
-#use str(InjOrder)???
 MetabInjOrder <- as.numeric(InjOrder$injection.order)
 InjOrderNames <- as.character(InjOrder$Name)
 InjTime <- InjOrder$Acquired
 Inj.df <- data.frame(Names = InjOrderNames, Order = MetabInjOrder, Time = InjOrder$Acquired, Case.control = InjOrder$Case.control)
-#([], ) trailing comma = do row stuff
 
-# InjFinal <- Inj.df[ !( grepl("T", Inj.df$Names) | (grepl("B", Inj.df$Names) ) ), ]
 InjFinal <- Inj.df[ !(is.na(InjOrder$Case.control)), ] #instead of removing stuff that isn't C/S, use an attrib of the data frame
 
 # - Doing logs and stuff
 LogMeasurements<-log(Metab)
 LogMeasurements<-as.matrix(LogMeasurements)
-CD<-matrix(nColumns, nBatch)
-#Batch<-SampInfo$Batch - Get from InjOrder, crossref w/ Sample Names
-#Cbind
-#polyorder.mat<-matrix(nColumns, nBatch) #do I actually need this?
-
-# - Organising data
-InjFinal2 <- InjFinal
 
 InjFinal$Names <- gsub("^C(\\d)$", "C0\\1", InjFinal$Names) #need to turn C1 -> C01 etc. for parity between Metab and InjOrder
 InjFinal$Names <- gsub("^S(\\d)$", "S0\\1", InjFinal$Names) #for S1 -> S01 etc.
@@ -80,24 +56,10 @@ RowMatch <- function(rr) {
 
 nrowsMetabNames <-dim(MetabRowNamesDataFrame)[1]
 Lorder <- 0
-Lorder2 <-0
 for (i in 1:nrowsMetabNames) {
   Lorder[i] <- RowMatch(i)
-  Lorder2[i] <- RowMatch(i)
 }
 Lorder <- as.numeric(Lorder)
-Lorder2 <- subset(Lorder, Type == 'C')
-
-TimeMatch <- function(tt) {
-  ForMatch <- as.character(MetabRowNamesDataFrame$Names[tt])
-  if(grep(ForMatch, InjectionOrderForMatch$Names, value = TRUE) == MetabRowNamesDataFrame$Names[tt]) {
-    zz <- grep(ForMatch, InjectionOrderForMatch$Names, value = FALSE)
-    MatrixToOrder <- c(InjectionOrderForMatch$Names[tt], as.character(InjectionOrderForMatch$Time[zz]))
-    cat(MatrixToOrder, "\n")
-    cat(InjectionOrderForMatch$Names[tt], "\t", InjectionOrderForMatch$Time[zz], "\n")
-  }
-}
-
 
 # - The below function makes a graph of non-log conc. vs samples
 mkSingleGraph <- function(d) { #d = no. of compound you want to plot
@@ -114,31 +76,14 @@ mkSingleGraphLog <- function(b) { #b = no. of compound you want to plot - log sc
   # - Plotting things out, inc. model
   mod1 <- lm(LogMeasurements[,b]~Lorder, subset=Type=='C')
   titleLog <- c(CompoundNames[,b], "R^2 = ", signif(summary(mod1)$r.squared,3) )
-  DetrendLogMeasurements <- subset(LogMeasurements[,b], Type == 'S') #subset "S" from LogMeasurements - mod1 
-  DetrendLogMeasurements <- as.matrix(DetrendLogMeasurements)
-  residbla2 <- resid(mod1)
-  residbla2 <- as.matrix(residbla2)
-  DetrendLogMeasurements <- DetrendLogMeasurements - abs(residbla2)
-  LogMeasurementsC <- subset(LogMeasurements[,b], Type == 'C')
   
   #COMPARE R2, PICK MODELS - pick best one for each compound? Or do all the same? Or write some code that evaluates R2, picks best model for each compound
   #DO PREDICTION FROM EVERYTHING, AND THEN SUBTRACT FROM OBSERVED (EVERYTHING)
   #THEN COMPARE CASES AND CONTROLS
   #Predict() looks for something in a dataframe with the same name as the var used in the model
   
-  # Lorder1 <- subset(Lorder, Type == 'C')
-  # Lorder2 <- subset(Lorder, Type == 'S')
-  # Lorder2 <- as.matrix(Lorder2)
-  # Lorder2 <- Lorder2[-1]
-  # cat(Lorder1, "\n", Lorder2)
-  #plot(Lorder, LogMeasurements[,b], main = titleLog, xlab = "Injection Order", ylab = "Log Relative Intensity", col=Type)
-  # plottystuff <- rbind(LogMeasurementsC, DetrendLogMeasurements)
-  # plot(Lorder, plottystuff, main = titleLog, xlab = "Injection Order", ylab = "Log Relative Intensity", col=Type)
-  # plot(Lorder2, DetrendLogMeasurements)
-# - Making a trendline
-      # linelog <- line(Lorder, LogMeasurements[,b])
-      # abline(coef(linelog))
-  # abline(mod1)
+  plot(Lorder, LogMeasurements[,b], main = titleLog, xlab = "Injection Order", ylab = "Log Relative Intensity", col=Type)
+  abline(mod1)
 }
 
 #if you don't want to use numbers!
@@ -206,56 +151,9 @@ BreakSplit <- 30
 
 #a logical value that will factor Lorder into two levels at the split
 
-mkModel2 <- function(b) { 
-  mod2 <- lm(Metab[,b] ~ BreakSplit, subset = Type == "C") #only using controls
-  cat(CompoundNames[,b], "R^2 = ", signif(summary(mod2)$r.squared,3))
-}
-
 mkModel2Log <- function(b) {
   mod2 <- lm(LogMeasurements[,b] ~ BreakSplit, subset = Type == "C")
   cat("LOG", CompoundNames[,b], "R^2 = ", signif(summary(mod2)$r.squared,3))
-}
-
-# - functions
-modelBreak <- function() {
-  nameinput <- readline(prompt = "Enter a compound name: ")
-  nameinput <- ifelse(grepl("[^A-Za-z0-9]", nameinput),NA,nameinput) # if the input isn't a text string, including digits, mark as NA
-  if (is.na(nameinput)) {
-    cat("'", nameinput, "'", "is not a valid text string for matching! Please try again.")
-  }
-  else
-    
-  collookup <- grep(nameinput, CompoundNames, ignore.case = TRUE, value = FALSE) #looks up column compound names, returns column number
-  # now need to sort out if grep returns more than one value (i.e. search "Alanine", get Phe and Ala)
-  alphabeta <- grep(nameinput, CompoundNames, ignore.case = TRUE, value = TRUE) #get the corresponding match names from a repeat grep
-  nMatches <- length(collookup) #store "length" (i.e. number) of lookups
-  
-  if (nMatches == 1) { #if length is equal to 1, there's only one option
-      mkModel2(collookup)
-      cat("\n")
-      mkModel2Log(collookup)
-  }
-  if (nMatches > 1) {  #if more than one match was obtained...
-    cat(nMatches, "matches found. Which option do you want to model?") #print a descriptive message asking for input
-    state <- 1 #just an iterator variable for the following loop
-    for(i in collookup){ #for as many values as collookup has (i.e. no. of matches returned)
-      cat("\n", " ", alphabeta[state], " ", "(",state,")") #print the match name, and number (for user input), each on a new line
-      state <- state+1 #increase the iterator so we move through the values
-    }
-    readline("Enter a positive integer: ") -> SelectiveInput #this is where we ask for input in the form of a +ve integer, corresponding to above matches
-    SelectiveInput <- ifelse(grepl("[^0-9]", SelectiveInput),NA,as.integer(SelectiveInput)) #if they don't give us a +ve integer, mark as NA
-    while (is.na(SelectiveInput)) { #As long as the input is set as "NA"
-      readline("Error. Please enter a positive integer: ") -> SelectiveInput #Throw up an error and ask them to try again
-      SelectiveInput <- ifelse(grepl("[^0-9]", SelectiveInput),NA,as.integer(SelectiveInput)) #Please get it right this time, user
-    }
-      mkModel2(collookup[SelectiveInput])
-      cat("\n")
-      mkModel2Log(collookup[SelectiveInput])
-  }
-  
-  if(nMatches == 0) { #if no matches found...
-    cat("No matches found for string", "'", nameinput, "'") #throw up an error message
-  }
 }
 
 # Modeling all compounds and producing output:
