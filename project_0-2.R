f45ab7c9e53d774b9ff4b71e07556db522f9d36c
kdea020 <kdea020@aucklanduni.ac.nz>
2016-09-08 11:27:41 +1200

    Working on implementing q-value statistic with the Bioconductor package qvalue (http://github.com/jdstorey/qvalue)

--- project_0-2.R
+++ project_0-2.R
@@ -1,7 +1,16 @@
 # - Basic instructions and data import
+#Import libraries:
+library(qvalue)
+
+#Do TDD stuff! Write the damn tests! And make program modular.
 
 WD <- getwd()
-cat("Working directory is:", WD)
+# cat("Working directory is:", WD)
+
+# For citation:
+citeQ <- "John D. Storey with contributions from Andrew J. Bass, Alan Dabney and David Robinson (2015). qvalue: Q-value estimation for false
+  discovery rate control. R package version 2.4.2. http://github.com/jdstorey/qvalue"
+# cat("This script uses the Q-value package from the open-source project Bioconductor:", "\n", citeQ, "\n")
 
 read.csv('Hair.csv')->Metab #load first data file with main readings
 read.csv('inj_order_SGA_hair.csv')->InjOrder #load injection order data file with equipment status
@@ -20,7 +29,7 @@ nRows<-dim(Metab)[1]
 # - Get C or S for each sample so can do just of one group
 Type<-substr(rownames(Metab),1,1) #Grab first letter of rownames
 Type[Type=='X']<-'Sample' #Define as either C or S for each sample
-Type<-factor(Type) #levels?
+Type<-factor(Type) #Coerce vector Type to a factor with two levels (C and S)
 
 orderA <- InjOrder$injection.order
 maxnRows <- max(nRows)
@@ -34,9 +43,8 @@ Inj.df <- data.frame(Names = InjOrderNames, Order = MetabInjOrder, Time = InjOrd
 
 InjFinal <- Inj.df[ !(is.na(InjOrder$Case.control)), ] #instead of removing stuff that isn't C/S, use an attrib of the data frame
 
-# - Doing logs and stuff
-LogMeasurements <- log(Metab)
-LogMeasurements <- as.matrix(LogMeasurements)
+LogMeasurements <- log10(Metab) #Perform log transformation on measurements
+LogMeasurements <- as.matrix(LogMeasurements) #Coerce the vector into a matrix for later use
 
 InjFinal$Names <- gsub("^C(\\d)$", "C0\\1", InjFinal$Names) #need to turn C1 -> C01 etc. for parity between Metab and InjOrder
 InjFinal$Names <- gsub("^S(\\d)$", "S0\\1", InjFinal$Names) #for S1 -> S01 etc.
@@ -76,14 +84,12 @@ mkSingleGraphLog <- function(b) { #b = no. of compound you want to plot - log sc
   # - Plotting things out, inc. model
   mod1 <- lm(LogMeasurements[,b]~Lorder, subset=Type=='C')
   titleLog <- c(CompoundNames[,b], "R^2 = ", signif(summary(mod1)$r.squared,3) )
-  
-  #COMPARE R2, PICK MODELS - pick best one for each compound? Or do all the same? Or write some code that evaluates R2, picks best model for each compound
-  #DO PREDICTION FROM EVERYTHING, AND THEN SUBTRACT FROM OBSERVED (EVERYTHING)
-  #THEN COMPARE CASES AND CONTROLS
-  #Predict() looks for something in a dataframe with the same name as the var used in the model
-  
   plot(Lorder, LogMeasurements[,b], main = titleLog, xlab = "Injection Order", ylab = "Log Relative Intensity", col=Type)
   abline(mod1)
+  X <- subset(LogMeasurements[,b], Type == "C")
+  Y <- subset(LogMeasurements[,b], Type == "S")
+  sig <- t.test(X, Y)
+  sig
 }
 
 #if you don't want to use numbers!
@@ -142,10 +148,11 @@ mkGraphName <- function() {
 
 # - Model 2: Before/After "break" (gap in days during experiment)
 IsBreak <- 36 # this will be a single value: the injection number of the sample done immediately before the break
-               # break was found at Sample C30 (94), So everything <=36 in the list is before the break (= TRUE).
+               # break was found at Sample C30 (94), So everything <=36 in the list is before the break (= TRUE)
+              #maybe make this modular with a readline() command later?
 Lorder.m <- as.matrix(Lorder)
 RowLorder.m <- row(Lorder.m)
-BreakSplit <- Lorder <= IsBreak
+BreakSplit <- Lorder <= IsBreak #The split is defined as a logical vector  
 BreakSplitC <- subset(BreakSplit, Type == "C")
 
 #a logical value that will factor Lorder into two levels at the split
@@ -156,8 +163,6 @@ LogMeasurementsS <- subset(LogMeasurements, Type == "S")
 
 Stairstep.df=data.frame(BreakSplitC = rep(0, length(LogMeasurements) ) )
 Stairstep.df=data.frame(BreakSplitC = BreakSplit)
-
-summy <- 0
 
 #From here on out, just looking at log(relative intensity) as a scaling normalisation step
 
@@ -191,20 +196,68 @@ GetAllR2 <- function() {
   }
 }
 
-# linear.df <- data.frame()
+linear.df = data.frame(LogMeasurementsC = rep(0, length(BreakSplit) ) )
+linear.df = data.frame(LogMeasurementsC = LogMeasurements)
 
-DoCorrection.Linear <- function() {
-  mod1 <- lm(LogMeasurements ~ Lorder, subset=Type=='C')
+finalvalues <- as.matrix(LogMeasurements[,1])
+
+DoCorrection.Linear <- function(i) {
+  mod1 <- lm(LogMeasurements[,i] ~ Lorder, subset=Type=='C')
   predict.mod1 <- predict(mod1, linear.df = linear.df)
+  finalvalues <- LogMeasurements[,i]  - predict.mod1
+  # cat(CompoundNames[,i], "\n", finalvalues[,i], "\n")
+}
+
+DoCorrection.Break <- function(i) {
+  mod2 <- lm(LogMeasurementsC[,i] ~ BreakSplitC)
+  predict.mod2 <- predict(mod2, Stairstep.df = Stairstep.df)
+  finalvalues <- LogMeasurements[,i]  - predict.mod2
+  # cat(predict.mod2, "\n")
+}
+
+# DoAllModels <- function() {
+  # cat("Compound", "\t", "LINEAR", "\t", "BREAK", "\n")
+
+LinearCorrections <- matrix(nrow = nRows, ncol = 1)
+BreakCorrections <- matrix(nrow = nRows, ncol = 1)
+nMetabs <- nColumns
+
+# for (i in seq_along(CompoundNames)) {
+LinearCorrectionGraph <- function(i) {
+  LinearCorrections <- DoCorrection.Linear(i)
+  plot(Lorder, LinearCorrections, main = CompoundNames[,i], xlab = "Injection Order", ylab = "Log Relative Intensity", col=Type)
+  C <- subset(LinearCorrections, Type == "C")
+  S <- matrix(subset(LinearCorrections, Type == "S"))
+  C <- 10^C
+  S <- 10^S
+  sig <- t.test(C, S, alternative = "t", mu = 0, paired = FALSE, var.equal = TRUE)
+  sig2 <- kruskal.test(LinearCorrections, Type)
+  cat("Linear model:", "\n", "T-test p-value:", sig$p.value, "\n", "Kruskal-Wallis p-value:", sig2$p.value, "\n", "\n") #prints both models' stats
 }
 
 
-DoCorrection.Break <- function() {
+BreakCorrectionGraph <- function(i) {
+  BreakCorrections <- DoCorrection.Break(i)
+  plot(Lorder, BreakCorrections, main = CompoundNames[,i], xlab = "Injection Order", ylab = "Relative Intensity", col=Type) #plot a graph
+  C <- subset(BreakCorrections, Type == "C")
+  S <- subset(BreakCorrections, Type == "S")
+  C <- 10^C #correcting for log values by exponent
+  S <- 10^S #as above, for cases instead of controls
+  sig <- t.test(C, S, alternative = "t", mu = 0, paired = FALSE, var.equal = TRUE) #Performs t-test on normalised data from break model
+  sig2 <- kruskal.test(BreakCorrections, Type) #Performs a Kruskal-Wallis non-parametric significance test
   
+  
+  # sig.pvalues <- empPvals(stat = , stat0 = , pool = FALSE)
+  # sig.q <- qvalue(p = sig.pvalues)
+  # sig2.q <- 
+  cat("Break model:", "\n", "T-test p-value:", sig$p.value, "\n", "T-test q-value:", sig,q, "\n", "Kruskal-Wallis p-value:", sig2$statistic, "\n", "\n") 
 }
 
-# Stairstep.Log <- ModelStairstep.Log(LogMeasurements) #residuals for subtraction for each [, compound#]
+ShowAndTell <- function(i) { #this function produces a set of graphs for a given compound
+mkSingleGraphLog(i)
+LinearCorrectionGraph(i)
+BreakCorrectionGraph(i)
+}
 
-# Modeling all compounds and producing output:
-#make a data frame
-#cat, concisely, elements of dataframe (Compound Name, Linear R^2, Break R^2, new line) for print
+#Make name input process modular, with compound number output going into any number of other specified functions?
+#Cite use of Bioconductor Q-value package in markdown and script! Use: ' citation("qvalue") '
